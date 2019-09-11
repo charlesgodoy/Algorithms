b027272fcde617f317108415c154e535fee98a31
charlesgodoy <charlesgodoy@live.com>
2019-09-11 18:09:43 -0400

    finished stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,24 @@
 
 import argparse
 
+# prices = [ 310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+# prices = [1050, 270, 1540, 3800, 2]
+prices = [100, 90, 80, 50, 20, 10]
+
 def find_max_profit(prices):
-  pass
+    # buy = 0
+    # sell = 0
+    max_difference = -10
+
+    
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            if prices[j] - prices[i] > max_difference:
+                max_difference = prices[j] - prices[i]
+
+    return max_difference
+
+print(find_max_profit(prices))
 
 
 if __name__ == '__main__':
